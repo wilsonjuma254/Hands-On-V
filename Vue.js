a1a4c03d5b7916eaf680e9b6d91414f238b08079
Wilsonmobisa <willexjumah12338@gmail.com>
2023-04-03 00:07:32 +0300

    mounted App

--- Vue.js
+++ Vue.js
@@ -1,3 +1,13 @@
 const App = Vue.createApp({
   // methods and content go here
+  //add data
+  data() {
+    return {
+      firstname: "Wil",
+      lastname: "Crat",
+    };
+  },
 });
+
+//mount the app to indicate the portion of the page the app controls
+App.mount("#app");
